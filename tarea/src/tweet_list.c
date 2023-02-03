a4eb54be48202e402e5bbeb941bb55fde7d4d62e
admeneses <alejandrocardie12@gmail.com>
2023-02-02 21:54:01 -0400

    add tweet_list.c user_list.c implementation

---
+++ tarea/src/tweet_list.c
@@ -0,0 +1,87 @@
+/**
+ * Implementacion de la lista circular, donde cada cada nodo es un
+ * apuntador a struct tweet.
+ *
+ * Soporta las operaciones de creacion y isercion.
+ */
+
+#include <stdlib.h>
+
+#include "user_list.h"
+#include "tweet_list.h"
+
+/**
+ * Crea una lista doblemente enlazada sin elementos.
+ *
+ * @return Un apuntador a la cabeza de la lista si la creacion fue exitosa. 
+ * de los contrario NULL.
+ */
+tweet_node *new_tweet_list() {
+    /* Asignar memoria dinamicamente para crear la lista */
+    tweet_node *head = malloc(sizeof(tweet_node));
+
+    if (!head)
+        return NULL;
+
+    /* La cabeza de la lista apunta a si misma */
+    head->next = NULL;
+    head->prev = NULL;
+    head->data = NULL;
+
+    return head;
+}
+
+/**
+ * Añade un tweet nodo a la lista.
+ *
+ * @param list: Apuntador a la direccion de la cabeza de la lista.
+ * @param data: Dato a almacenar en el nodo, de tipo User.
+ * @return 1 si la operación fue exitosa. De lo contrario 0.
+ */
+u_int8_t push_tweet_list(tweet_node **list, tweet *data) {
+    tweet_node *head = *list;
+
+    if (!head->data) {
+        /* Si la lista es nueva, coloca el dato en el nodo */
+        head->data = data;
+    } else {
+        /* De lo contrario, crea una nueva entrada para la lista
+        dinamicamente */
+        tweet_node *new_node = malloc(sizeof(tweet_node));
+
+        if (!new_node)
+            return 0;
+
+        /* Coloca el dato en el nodo */
+        new_node->data = data;
+        
+        /* Recorre la lista enlazada hasta el final */
+        while (head->next)
+            head = head->next;
+
+        /* head es el ultimo nodo de la lista enlazada */
+        head->next = new_node;
+        new_node->prev = head;
+    }
+
+    return 1;
+}
+
+/*
+ * Crea una structura tweet
+ *
+ * @param str_tweet: str del tweet.
+ * @param tm: hora y fecha de publicacion del tweet.
+ * @return Un apuntador a tweet si la creacion fue exitosa.
+ *         NULL en caso contrario.
+ */
+tweet* new_tweet(char* str_tweet, time_t tm) {
+    tweet *tw = malloc(sizeof(tweet));
+
+    if (!tw)
+        return NULL;
+
+    tw->str_tweet = str_tweet;
+    tw->tm = tm;
+    return tw;
+}
---
+++ tarea/src/user_list.c
@@ -0,0 +1,87 @@
+/**
+ * Implementacion de la lista circular, donde cada cada nodo es un
+ * apuntador a struct User.
+ *
+ * Soporta las operaciones de creacion y isercion.
+ */
+
+#include <stdlib.h>
+#include "user_list.h"
+#include "tweet_list.h"
+
+/**
+ * Crea una lista doblemente enlazada sin elementos.
+ *
+ * @return Un apuntador a la cabeza de la lista si la creacion fue exitosa. 
+ * de los contrario NULL.
+ */
+user_node *new_user_list() {
+    /* Asignar memoria dinamicamente para crear la lista */
+    user_node *head = malloc(sizeof(user_node));
+
+    if (!head)
+        return NULL;
+
+    /* La cabeza de la lista apunta a si misma */
+    head->next = NULL;
+    head->prev = NULL;
+    head->data = NULL;
+
+    return head;
+}
+
+/**
+ * Añade un user nodo a la lista.
+ *
+ * @param list: Apuntador a la direccion de la cabeza de la lista.
+ * @param data: Dato a almacenar en el nodo, de tipo User.
+ * @return 1 si la operación fue exitosa. De lo contrario 0.
+ */
+u_int8_t push_user_list(user_node **list, user *data) {
+    user_node *head = *list;
+
+    if (!head->data) {
+        /* Si la lista es nueva, coloca el dato en el nodo */
+        head->data = data;
+    } else {
+        /* De lo contrario, crea una nueva entrada para la lista
+        dinamicamente */
+        user_node *new_node = malloc(sizeof(user_node));
+        if (!new_node)
+            return 0;
+
+        /* Coloca el dato en el nodo */
+        new_node->data = data;
+
+        new_node->next = head;
+        new_node->prev = NULL;
+        head->prev = new_node;
+
+        /* El nuevo node es la cabeza de la lista */
+        *list = new_node;
+    }
+
+    return 1;
+}
+
+/*
+ * Crea una structura user
+ *
+ * @param username: nombre de usuario del usuario.
+ * @param hash_password: hash de la contrasena del usuario.
+ * @return Un apuntador a User si la creacion fue exitosa.
+ *         NULL en caso contrario.
+ */
+user* new_user(char* username, int hash_password) {
+    user *u = malloc(sizeof(user));
+
+    if (!u)
+        return NULL;
+
+    u->username = username;
+    u->hash_password = hash_password;
+    u->tweet_list = new_tweet_list(); /* Crear lista de tweets */
+    u->sig_list = new_user_list(); /* Crear lista de users */
+
+    return u;
+}
