2287e27d84a8af302abc146af53f7665bcfa75a3
admeneses <alejandrocardie12@gmail.com>
2023-02-11 00:30:34 -0400

    update main

--- tarea/src/main.c
+++ tarea/src/main.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "user_list.h"
-#include "tweet_list.h"
 #include "hash_table.h"
-#include "main_screen.h"
+#include "screens.h"
 
 int main() {
     user_node **t = new_hash_table();
-    /* user *u0 = new_user("admeneses", 5); */
-    /* printf("%s\n", u0->username); */
-    /*printf("%d\n", hash_code("admeneses\0")); */
-    /*insert_user_hash_table(&t, u0); */
-    /*insert_user_hash_table(&t, u0); */
-    /*insert_user_hash_table(&t, u0); */
-    /*printf("%d\n", hash_code(u0->username)); */
-    /*printf("%s\n", t[9]->next->next->data->username); */
-    main_screen(t);
+    screen1(t);
     free_table(t);
     return 0;
 }
