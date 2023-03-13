d02981795f86a85ec451cab261181e8baa8e8827
admeneses <alejandrocardie12@gmail.com>
2023-03-12 21:27:53 -0400

    fixed bug

--- proyecto_1/src/main.c
+++ proyecto_1/src/main.c
@@ -4,8 +4,8 @@
 
 
 int main(int argc, char *argv[]) {
-    svc_node *svc_list = new_stop_list();
-    stop_node *stop_list = new_service_list();
+    svc_node *svc_list = new_service_list();
+    stop_node *stop_list = new_stop_list();
 
     read_input(argc, argv, &svc_list, &stop_list);
 	return 0;
