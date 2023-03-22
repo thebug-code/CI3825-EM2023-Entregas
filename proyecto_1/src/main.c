cc1c1b3ed0fca0d7009778f97d4db545a25adfd3
admeneses <alejandrocardie12@gmail.com>
2023-03-22 01:45:08 -0400

    implementada simulacion del tiempo

--- proyecto_1/src/main.c
+++ proyecto_1/src/main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <sys/wait.h>
@@ -12,6 +13,10 @@
 #define WRITE_END 1
 #define MESSAGE_SIZE 256
 
+
+int nanosleep(const struct timespec *req, struct timespec *rem);
+
+
 /* Estructura para almacenar los argumentos de la función simulate_bus_route */
 typedef struct Arg_struct {
     int bus_id; /* Identificador del autobús */ 
@@ -19,6 +24,7 @@ typedef struct Arg_struct {
     svc_node *arg1; /* Apuntador a la cabeza de la lista de servicios */
     stop_node *arg2; /* Apuntador a la cabeza de la lista de paradas */
 } args;
+
 
 /* 
  * Crea una estructura args con los argumentos de la función 
@@ -52,23 +58,71 @@ args *create_new_args(int bus_id,
 
 
 /*
+ * Crea una estructura tm con la hora actual y la modifica
+ * para que tenga la hora, minuto y segundo especificados.
+ *
+ * @param hour Hora
+ * @param min Minuto
+ * @param sec Segundo
+ * @return Estructura tm con la hora especificada
+ */
+time_t create_hour(int hour, int min, int sec) {
+    time_t t;
+    struct tm *time_info;
+
+    t = time(NULL);
+    time_info = localtime(&t);
+
+    time_info->tm_hour = hour;
+    time_info->tm_min = min;
+    time_info->tm_sec = sec;
+    t = mktime(time_info);
+
+    return t;
+}
+
+
+/*
  * Simula el recorrido de un autobús por una ruta.
  *
  * @param arg Apuntador a la estructura args
  */
 void *simulate_bus_route(void *arg) {
     args *info = (args *) arg;
+    char message[MESSAGE_SIZE];
     int bus_id = info->bus_id;
     int *fd = info->fd;
 
     close(fd[READ_END]); /* Cierra el extremo de lectura no utilizado */
 
     /* Escribe un mensaje en el pipe */
-    char message[MESSAGE_SIZE];
-    sprintf(message, "Hola, soy el hilo %lu\n", pthread_self());
+    sprintf(message, "Bus %d: Hello world!", bus_id);
     write(fd[WRITE_END], message, sizeof(message));
 
     pthread_exit(NULL);
+}
+
+
+/*
+ * Obtiene el tiempo actual de la simulación.
+ *
+ * @param start_time Tiempo de inicio de la simulación
+ * @param sim_counter Contador de tiempo de la simulación
+ * @return Tiempo actual de la simulación
+ */
+time_t get_current_time(time_t start_time, size_t sim_counter) {
+    struct tm *current_time;
+    time_t current_time_t;
+
+    /* Convertir start_time a struct tm */
+    current_time = localtime(&start_time);
+
+    current_time->tm_min += sim_counter;
+
+    /* Convertir de vuelta a time_t */
+    current_time_t = mktime(current_time);
+
+    return current_time_t;
 }
 
 
@@ -90,19 +144,11 @@ int main(int argc, char *argv[]) {
     int **pipefd = initialize_pipes(num_routes);
 
     /* Variables para el manejo de tiempo de la simulación */
-    int sim_time = 28800; /* 8 horas de servicio en segundos */
-    int sim_counter = 0; /* contador de tiempo de la simulación */
-    time_t start_time;
-    struct tm *start_time_info;
+    size_t sim_time = 480; /* 8 horas de servicio en minutos */
+    size_t sim_counter = 0; /* contador de tiempo (en mins) de la simulación */
     
-    start_time = time(NULL); /* Se obtiene la hora actual */
-    start_time_info = localtime(&start_time); /* Se convierte la hora actual a una estructura tm */
-    
-    /* Inicializa la hora de inicio del servicio a las 6:00 */
-    start_time_info->tm_hour = 6;
-    start_time_info->tm_min = 0;
-    start_time_info->tm_sec = 0;
-    start_time = mktime(start_time_info); /* Convierte la hora de inicio del servicio a time_t */
+    /* Inicializa la hora de inicio del servicio a las 4:10 */
+    time_t start_time = create_hour(4, 10, 0);
 
     /* Crea los procesos hijos */
     int i;
@@ -114,50 +160,79 @@ int main(int argc, char *argv[]) {
             exit(EXIT_FAILURE);
         } 
         else if (!child_pids[i]) {
-            /* i-esimo proceso hijo FALTA LOS FREE */
+            /* i-esimo proceso hijo */
 
-            /* Crear el hilo y ejecutar la función simulate_bus_route */
-            pthread_t bus_thread_id;
-            if (pthread_create(&bus_thread_id, NULL, simulate_bus_route, 
-                create_new_args(i, pipefd[i], svc_list, stop_list))) {
-                perror("pthread_create");
-                exit(EXIT_FAILURE);
+            /* id del autobús */
+            int j = 0;
+            /* Puntero a la lista de horarios de los k-esimo autobuses */
+            sched_node *sched_list = svc_list->data->scheds;
+
+            /* Inicio de la simulacion */
+            while (sim_counter < sim_time) {
+                struct timespec ts; /* Estructura para el nanosleep */
+                time_t current_time; /* Tiempo actual de la simulación */
+                char message[MESSAGE_SIZE]; /* Mensaje a escribir en el pipe */
+
+                /* Obtiene el tiempo actual de la simulación */
+                current_time = get_current_time(start_time, sim_counter);
+                if (!difftime(current_time, sched_list->data->time)) {
+                    /* Si el tiempo actual es igual al tiempo de salida del autobús */
+                    /* Crea un hilo para simular el recorrido del autobús */
+                    pthread_t bus_thread_id;
+                    if (pthread_create(&bus_thread_id, NULL, simulate_bus_route, 
+                        create_new_args(j, pipefd[i], svc_list, stop_list))) {
+                        perror("pthread_create");
+                        exit(EXIT_FAILURE);
+                    }
+
+                    /* Cerrar el extremo de lectura no utilizado */
+                    close(pipefd[i][READ_END]); 
+
+                    /* Escribir un mensaje en el pipe */
+                    sprintf(message, "Hola, soy la ruta %s", svc_list->data->route);
+                    write(pipefd[i][WRITE_END], message, sizeof(message));
+
+                    /* Espera a que termine el hilo */
+                    if (pthread_join(bus_thread_id, NULL)) {
+                        perror("pthread_join");
+                        exit(EXIT_FAILURE);
+                    }
+
+                    /* Avanza al siguiente horario */
+                    sched_list = sched_list->next;
+                    j++;
+                }
+
+                /* Simula el paso del tiempo */
+                ts.tv_sec = 0;
+                ts.tv_nsec = min_duration * 1000; /* min_duration en microsegundos */
+                nanosleep(&ts, NULL);
+                
+                /* Incrementa el contador de tiempo de la simulación */
+                sim_counter++;
             }
-
-            close(pipefd[i][READ_END]); /* Cerrar el extremo de lectura no utilizado */
-
-            /* Escribir un mensaje en el pipe */ 
-            char message[MESSAGE_SIZE];
-            sprintf(message, "Hola, soy el proceso hijo %d", i);
-            write(pipefd[i][WRITE_END], message, sizeof(message));
-
-            /* Espera a que termine el hilo */
-            if (pthread_join(bus_thread_id, NULL)) {
-                perror("pthread_join");
-                exit(EXIT_FAILURE);
-            }
-
             /* Salir del proceso hijo */
             exit(EXIT_SUCCESS);
-        } else {
+        } 
+        else {
             /* Proceso padre */
-            
-            close(pipefd[i][WRITE_END]); /* Cerrar el extremo de escritura no utilizado */
-
-            /* Leer el mensaje enviado por el proceso hijo */
+            int status;
             char buffer[MESSAGE_SIZE];
             int n;
+
+            /* Cerrar el extremo de escritura no utilizado */
+            close(pipefd[i][WRITE_END]); 
+
+            /* Leer el mensaje enviado por el proceso hijo */
             while ((n = read(pipefd[i][READ_END], buffer, sizeof(buffer))) > 0)
                 printf("Mensaje recibido del proceso hijo %d: %s\n", i, buffer);
 
             /* Esperar a que termine el i-esimo proceso hijo */
-            int status;
             waitpid(child_pids[i], &status, 0);
+
+            svc_list = svc_list->next;
+            stop_list = stop_list->next;
         }
-
-        svc_list = svc_list->next;
-        stop_list = stop_list->next;
     }
-
     return 0;
 }
